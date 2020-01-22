1ac9a5c9e5feac3046b84a33e2e61dbf37b38113
Karen k  sin <karenksin@MacBook-Pro.attlocal.net>
2020-01-22 01:41:39 -0800

    Updates

---
+++ precache-manifest.cbeab89af3ebfa96edd20f1c0443bf25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0501d9dabce078112456e579f35decf",
+    "url": "/toeWars/index.html"
+  },
+  {
+    "revision": "6cc9665a947c3136b2fb",
+    "url": "/toeWars/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "371753da654144f26498",
+    "url": "/toeWars/static/css/main.5162be38.chunk.css"
+  },
+  {
+    "revision": "6cc9665a947c3136b2fb",
+    "url": "/toeWars/static/js/2.46f6512b.chunk.js"
+  },
+  {
+    "revision": "8a14dcea7ad8b593c1e83c11f7ba1a89",
+    "url": "/toeWars/static/js/2.46f6512b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "371753da654144f26498",
+    "url": "/toeWars/static/js/main.52b66ce1.chunk.js"
+  },
+  {
+    "revision": "565ab6a86c5f1e8dabec",
+    "url": "/toeWars/static/js/runtime-main.b9d7a79f.js"
+  }
+]);
