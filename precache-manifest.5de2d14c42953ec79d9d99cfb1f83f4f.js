7cc63bbc53d6dda94e0294b4bf1d451b670cc376
Karen k  sin <karenksin@MacBook-Pro.attlocal.net>
2020-01-22 10:37:26 -0800

    Updates

--- precache-manifest.5de2d14c42953ec79d9d99cfb1f83f4f.js
+++ precache-manifest.5de2d14c42953ec79d9d99cfb1f83f4f.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5be0148eb5750351c01c73b87c39b8cd",
+    "revision": "1030c26b93d0282bc5f32f5ebc141e20",
     "url": "/toeWars/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/toeWars/static/css/2.17e5ed98.chunk.css"
   },
   {
-    "revision": "9670db4f6d1f25373873",
-    "url": "/toeWars/static/css/main.5162be38.chunk.css"
+    "revision": "f1910a75f7f17f235685",
+    "url": "/toeWars/static/css/main.949b8116.chunk.css"
   },
   {
     "revision": "a17d952c3ea88e062950",
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/toeWars/static/js/2.67375a8c.chunk.js.LICENSE"
   },
   {
-    "revision": "9670db4f6d1f25373873",
+    "revision": "f1910a75f7f17f235685",
     "url": "/toeWars/static/js/main.c87aac5c.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/toeWars/precache-manifest.3e5a7ef047f889895e611df194209dc9.js"
+  "/toeWars/precache-manifest.5de2d14c42953ec79d9d99cfb1f83f4f.js"
 );
 
 self.addEventListener('message', (event) => {
