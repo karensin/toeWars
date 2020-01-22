3a6a013c15676a59a8e98a07da1f505a244b38dd
Karen k  sin <karenksin@MacBook-Pro.attlocal.net>
2020-01-22 02:01:03 -0800

    router

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import App from './App.js';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
+import Main from './components/Main.js';
 
 
 
 ReactDOM.render(
+  <BrowserRouter basename="/toeWars">
 
-  <App />,
+    <Route exact path="/" component={App} />
 
-  document.getElementById('root')
-);
+
+  </BrowserRouter>
+
+
+  , document.getElementById('root'));
 
